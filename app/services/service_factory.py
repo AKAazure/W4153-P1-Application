2334c892c61eb541c8d77d0cdaa3552ee28f6594
Akazure <yuxuz23@uci.edu>
2024-09-18 00:43:28 -0400

    p0

--- app/services/service_factory.py
+++ app/services/service_factory.py
@@ -1,6 +1,7 @@
 from framework.services.service_factory import BaseServiceFactory
 import app.resources.course_resource as course_resource
 from framework.services.data_access.MySQLRDBDataService import MySQLRDBDataService
+import yaml
 
 
 # TODO -- Implement this class
@@ -17,7 +18,11 @@ class ServiceFactory(BaseServiceFactory):
         if service_name == 'CourseResource':
             result = course_resource.CourseResource(config=None)
         elif service_name == 'CourseResourceDataService':
-            context = dict(user="root", password="dbuserdbuser",
+            with open("app/config/database.yaml", "r") as file:
+                db_config = yaml.load(file, Loader=yaml.FullLoader)
+            user  = db_config["user"] if "user" in db_config else "root"
+            password = db_config["password"] if "password" in db_config else "dbuserdbuser"
+            context = dict(user=user, password=password,
                            host="localhost", port=3306)
             data_service = MySQLRDBDataService(context=context)
             result = data_service
